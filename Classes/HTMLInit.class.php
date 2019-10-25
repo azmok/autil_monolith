a28516e09155153ca2402ac15cf9cb64d7e15ae9
Azuma Okumura <lambda-101@email.plala.or.jp>
2019-10-25 19:27:55 +0900

    HTMLInit class added.

---
+++ Classes/HTMLInit.class.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace OOPe;
+
+
+
+
+
+
+
+/** Initilizing HTML for PHP Dev 
+ *
+ */
+class HTMLInit{
+/*----------------------
+      HTMLInit
+------------------------
+   
+------------------------
+   + _init
+   + insertHTML
+   + loadCss
+   + loadJs
+   + renderJs
+   + appendInlineStyle
+----------------------*/
+
+   function __construct(){
+      $this->_init();
+      
+      $this->viewport();
+      $this->loadCss( PATH_CSS_BOOTSTR );
+   }
+   
+   
+   /*
+    *
+    */
+   function _init(){
+      $js = <<<DOC
+;(function( window ){
+   const _  = function(...args){
+      console.log(...args);
+   },
+   doc = document,
+   docRoot = doc.documentElement,
+   body = doc.body;
+   
+   const initHTML = function(tagName){
+      elm = doc.createElement( tagName );
+   
+      elm.innerHTML = "";
+      
+      if( tagName.toLowerCase() === "html" ){
+         doc.documentElement = html;
+      } else {
+         doc[tagName] = elm;
+      }
+   },
+   assertHTMLCore  = function(){
+      if( !docRoot ){
+         _(1);
+         initHTML( "html" );
+      }
+      if( !doc.head ){
+         _(2);
+         initHTML( "head" );
+      }
+      if( !body ){
+         _(3);
+         initHTML( "body" );
+      }
+   }
+})( window );
+
+let _  = function(...args){
+   console.log(...args);
+},
+docRoot = document.documentElement;
+_( docRoot.outerHTML );
+DOC;
+   
+      $this->renderJs($js);
+   }
+   
+   
+   /**
+    *
+    */
+   function viewport(){
+      $str = "<meta name='viewport' content='width=device-width, initial-scale=1' />";
+      
+      $this->insertHTML("head", $str);
+   }
+   
+   
+   /**
+    *
+    */
+   function insertHTML($tagName, $txt){
+      $js = <<<DOC
+let elm = document.querySelector("$tagName");
+
+elm.innerHTML += "$txt";
+DOC;
+   
+      $this->renderJs($js);
+   }
+   
+
+   /**
+    *
+    */
+   function loadCss($url){
+      $html = "<link rel='stylesheet' href='{$url}'>";
+      
+      $this->insertHTML("head", $html);
+   }
+   
+   
+   /**
+    *
+    */
+   function loadJs($url=""){
+      $js = <<<DOC
+let body = document.body;
+let elm = document.createElement("SCRIPT");
+
+elm.src = "{$url}";
+body.appendChild(elm);
+DOC;
+   
+      $this->renderJs($js);
+   
+   }
+   
+   
+   /**
+    *
+    */
+   function renderJs($js){
+      $jsTemplate = <<<DOC
+<script>     
+"use strict";
+
+window.addEventListener("DOMContentLoaded", ()=>{
+   {$js}
+});
+</script>
+DOC;
+   
+      echo $jsTemplate ."\n";
+   }
+   
+   
+   /**
+    *
+    */
+   function addStyle($tagName, $assoc){
+      $json = json_encode($assoc);
+      
+      $js = <<<DOC
+let el = document.querySelector("$tagName"),
+props = $json;
+      
+for(let prop in props){
+   el.style[prop] = props[prop];
+}
+DOC;
+   
+      $this->renderJs($js);
+   }
+}
+
+
+   
+
+   
+
+   
+
+   
+
+   
+
+   
+
+   
+
+   
+
+   
--- html.php
+++ html.php
@@ -85,6 +85,9 @@ function create($txt, $tagName="div", $assocArr=[]){
 }
 
 
+/**
+ *
+ */
 function escape($str, ...$ops){
    $flags = $ops[0] ? $ops[0] : ENT_COMPAT;
    $encoding = $ops[1] ? $ops[1] : ini_get("default_charset");
@@ -94,104 +97,12 @@ function escape($str, ...$ops){
 }
 
 
+/**
+ *
+ */
 function unescape($str, ...$flags){
    $flags = $flags[0] ? $flags[0] : ENT_COMPAT | ENT_HTML401;
    return htmlspecialchars_decode($str, $flags);
-}
-
-
-
-
-
-
-
-
-
-/*  */
-
-/**
- *
- */
-function injectJs($js){
-   $jsTemplate = <<<JsTemp
-<script>     
-"use strict";
-window.addEventListener("load", ()=>{
-   {$js}
-});
-</script>
-JsTemp;
-
-   echo $jsTemplate ."\n";
-}
-
-
-/**
- *
- */
-function insertIntoHeadTag ($str){
-   $js = <<<JSCOMP
-let head = document.querySelector("head");
-
-head.innerHTML += "{$str}";
-JSCOMP;
-
-   injectJs($js);
-}
-
-
-/**
- *
- */
-function appendViewPort (){
-   $str = "<meta name='viewport' content='width=device-width, initial-scale=1' />";
-   insertIntoHeadTag($str);
-}
-
-
-
-/**
- *
- */
-function loadCss($url=PATH_CSS_BOOTSTR){
-   $str = "<link rel='stylesheet' href='{$url}'>";
-   
-   insertIntoHeadTag($str);
-}
-
-
-/**
- *
- */
-function loadJs($url=""){
-   $js = <<<DOC
-let body = document.body;
-let elm = document.createElement("SCRIPT");
-
-elm.src = "{$url}";
-body.appendChild(elm);
-
-DOC;
-
-   injectJs($js);
-
-}
-
-
-/**
- *
- */
-function appendInlineStyle($elm, $props){
-   $js = <<<APPENDOC
-let el = document.querySelector("$elm");
-let props = $props;
-      
-for(let prop in props){
-   el.style[prop] = props[prop];
-}
-APPENDOC;
-
-   injectJs($js);
 }
 
 
@@ -209,8 +120,38 @@ function highlight($str){
 
 
 
-/*** initiliaze HTML meta vireport, css  **/
-appendViewPort();
-loadCss();
-appendInlineStyle("body", "{padding: '2rem'}" );
-/*********************************/
+
+/*****  initiliazing HTML  ********
+   - settingviewport,
+   - loading BootStrup.css
+   - Styling Body element
+/*******************************/
+(new \OOPe\HTMLInit())
+   ->addStyle("body", [
+         "padding" => "2rem",
+   ]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
